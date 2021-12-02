032808d8b2f74982312da644d002d1815524618b
d.richardcarl <d.richardcarl@gmail.com>
2021-12-02 16:26:33 +0800

    set initial leverage to max

--- binance.py
+++ binance.py
@@ -164,7 +164,9 @@ class BinanceBot(Bot):
         await self.update_position()
 
     async def execute_leverage_change(self):
-        lev = 25 # arbitrary
+        # set initial lev to max
+        data = await self.private_get(self.endpoints['leverage_bracket'], {'symbol': self.symbol})
+        lev = data[0]['brackets'][0]['initialLeverage']
         return await self.private_post(self.endpoints['leverage'], {'symbol': self.symbol, 'leverage': lev})
 
     async def init_exchange_config(self) -> bool:
